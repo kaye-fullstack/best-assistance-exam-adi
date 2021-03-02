745c984c957d7e8edb08df87ba143fd601492798
adi-fullstack <adi@fullstack.ph>
2021-03-02 17:27:25 +0800

    initial commit

---
+++ src/scripts/modules/theme-module.js
@@ -0,0 +1,30 @@
+AppName.Modules.ThemeModule = (function () {
+  //Dependencies
+  var core = AppName.Core;
+
+  //////////////////////
+  // Private Methods //
+  ////////////////////
+  const _privateMethod = () => {
+    // private stuff
+
+    const swiper = new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 0,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  };
+
+  /////////////////////
+  // Public Methods //
+  ///////////////////
+  const init = function () {
+    _privateMethod();
+  };
+
+  return {
+    init: init,
+  };
+})();
